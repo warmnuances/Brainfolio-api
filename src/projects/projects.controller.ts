96854b7df9d1dc97ba52296c1c04bd6daa8d481c
William <Wintan@student.unimelb.edu.au>
2020-10-02 06:02:41 +1000

    Upload Attempt Firebase

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -3,6 +3,9 @@ import { ProjectDto } from './dto/create-project.dto';
 import { ProjectsService } from './projects.service'
 import { Project } from './interfaces/project.interface'
 import {FileFieldsInterceptor} from '@nestjs/platform-express'
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from '../utils/file-uploading.utils';
+
 
 import * as admin from 'firebase-admin';
 
@@ -26,21 +29,37 @@ export class ProjectsController {
     }
     
     @Post('upload')
-    @UseInterceptors(FileFieldsInterceptor([
-      { name: 'avatar', maxCount: 1 },
-      { name: 'background', maxCount: 1 },
-    ]))
-
-
-    uploadFile(@UploadedFiles() files, @Body() theRest: String) {
+    @UseInterceptors(FileFieldsInterceptor(
+            [
+                { name: 'avatar', maxCount: 1 },
+                { name: 'background', maxCount: 1 },
+            ],
+            {
+                storage: diskStorage({
+                  destination: './files',
+                  filename: editFileName,
+                }),
+                fileFilter: imageFileFilter,
+              }
+        )
+        
+    )
+    async uploadMultipleFiles(@UploadedFiles() files) {
+        const response = [];
+        files.background.forEach(file => {
+          const fileReponse = {
+            filename: file.filename,
+          };
+          response.push(fileReponse);
+        });
 
         var bucket = admin.storage().bucket();
 
-        console.log(bucket);
-         
-        async function uploadFile() {
+        async function uploadFile(fileName) {
+
+            var imgPath = './files/'+fileName
             // Uploads a local file to the bucket
-            await bucket.upload(files, {
+            await bucket.upload(imgPath, {
               // Support for HTTP requests made with `Accept-Encoding: gzip`
               gzip: true,
               // By setting the option `destination`, you can change the name of the
@@ -52,15 +71,51 @@ export class ProjectsController {
                 cacheControl: 'public, max-age=31536000',
               },
             });
+        
+            console.log(` uploaded to brainfolio-1faf6`);
+        }
+         console.log(response[0].filename);
           
-            console.log(`${files} uploaded to brainfolio-1faf6`);
+        uploadFile(response[0].filename).catch(console.error);
+
+        console.log(response);
+        
+        return response;
+    }
+    @Get(':imgpath')
+    uploadFile(@Param('imgpath') image, @UploadedFiles() files, @Body() theRest: String) {
+        
+        
+        var bucket = admin.storage().bucket();
+
+        var imgPath = './files/' + image
+
+        console.log(imgPath);
+        // console.log(bucket);
+         
+        async function uploadFile() {
+            // Uploads a local file to the bucket
+            await bucket.upload(imgPath, {
+              // Support for HTTP requests made with `Accept-Encoding: gzip`
+              gzip: true,
+              // By setting the option `destination`, you can change the name of the
+              // object you are uploading to a bucket.
+              metadata: {
+                // Enable long-lived HTTP caching headers
+                // Use only if the contents of the file will never change
+                // (If the contents will change, use cacheControl: 'no-cache')
+                cacheControl: 'public, max-age=31536000',
+              },
+            });
+        
+            console.log(` uploaded to brainfolio-1faf6`);
         }
           
         uploadFile().catch(console.error);
-        console.log(theRest);
+        // console.log(theRest);
 
         
-        console.log(files);
+        // console.log(files);
     }
 
     
