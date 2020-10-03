c1d29b497aafb84fe74fc22febd8fda0617436e6
hlynn <hlynn@student.unimelb.edu.au>
2020-10-04 04:36:54 +1100

    fixed import

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -3,7 +3,7 @@ import { PortfolioController } from './portfolio.controller';
 import { PortfolioService } from './portfolio.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PortfolioSchema } from './schemas/portfolio.schema';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Portfolio', schema: PortfolioSchema }]), AuthModule],
