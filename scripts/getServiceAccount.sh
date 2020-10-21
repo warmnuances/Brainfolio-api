63f96e8cfe38a2657f59a4f8a14c8922db497641
hlynn <hlynn@student.unimelb.edu.au>
2020-10-22 00:04:18 +1100

    Service account fix

--- scripts/getServiceAccount.sh
+++ scripts/getServiceAccount.sh
@@ -1,1 +1,11 @@
-echo $FIREBASE_APPLICATION_CREDENTIALS > google-credentials.json
+file=google-credentials.json
+
+# -f Check if file exists
+if [ -f $file ] ; then
+    rm $file
+fi
+
+echo $FIREBASE_APPLICATION_CREDENTIALS_STRING > $file
+
+export FIREBASE_APPLICATION_CREDENTIALS="`pwd`/${file}"
+
