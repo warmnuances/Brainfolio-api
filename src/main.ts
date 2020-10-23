1e6eb746db18eafd79a8a04d65adb0c11f1f89b6
hlynn <hlynn@student.unimelb.edu.au>
2020-10-23 14:48:20 +1100

    Service account fix

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  console.log(process.env.FIREBASE_APPLICATION_CREDENTIALS);
 
   admin.initializeApp({
     credential: admin.credential.cert(process.env.FIREBASE_APPLICATION_CREDENTIALS),
